02b5544fc167d41b098b4461a286326609fdd340
Graham Baradoy <graham.baradoy@pixelunion.net>
2019-07-22 10:02:37 -0700

    Remove `test` as default env for CircleCi

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,5 @@
 [
-  # Phoenix.LiveReloader is :dev only
+  # Phoenix.LiveReloader is not reccognized by dialyzer
   {":0:unknown_function Function Phoenix.LiveReloader.call/2 does not exist."},
   {":0:unknown_function Function Phoenix.LiveReloader.init/1 does not exist."}
 ]
